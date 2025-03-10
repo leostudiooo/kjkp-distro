abd7d8b5c145b4d721812ff97d72ab4bc3fc6b12
leostudiooo <lilingfeng0408@outlook.com>
2025-03-10 22:08:33 +0800

    fix: 更新许可证链接文本以更准确地反映内容

--- src/components/License.vue
+++ src/components/License.vue
@@ -4,7 +4,7 @@
             <h3>使用规约</h3>
         </summary>
         <div class="content">
-            <p>本站分发的内容可以自由下载和使用。以任何形式使用本站内容时，都请注明出处（除非在文件中另有说明），并以相同形式共享。不得将下载的内容用于商业用途。使用规范细则请参考 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" target="_blank" rel="license noopener noreferrer">CreativeCommons 署名—非商业性使用—相同方式共享 4.0</a> 许可协议，请务必仔细阅读并遵守。</p>
+            <p>本站分发的内容可以自由下载和使用。以任何形式使用本站内容时，都请注明出处（除非在文件中另有说明），并以相同形式共享。不得将下载的内容用于商业用途。使用规范细则请参考 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" target="_blank" rel="license noopener noreferrer">知识共享 署名—非商业性使用—相同方式共享 4.0</a> 许可协议，请务必仔细阅读并遵守。</p>
         </div>
     </details>
 </template>
